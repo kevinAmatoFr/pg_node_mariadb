9ad5acd6de9a6c3c995cba6e3bca606929696ae1
Kevin Amato <kevin.amato@two-i.fr>
2022-05-18 16:42:03 +0200

    update data

---
+++ data/mock_co_character.js
@@ -0,0 +1,86 @@
+const races = {
+    nom: 'Demi-elfe',
+    nom: 'Demi-orque',
+    nom: 'Haut-elfe',
+    nom: 'Sylvain-Elfe',
+    nom: 'Gnome',
+    nom: 'Halfelin',
+    nom: 'Humain',
+    nom: 'Nain'
+};
+
+const profils = {
+    arquebusier: {
+        nom: 'Arquebusier',
+        voies: ['Voie de l’artilleur', 'Voie des explosifs', 'Voie du champ de bataille', 'Voie du pistolero', 'Voie de la précision']
+    },
+    barbare: {
+        nom: 'Barbare',
+        voies: ['Voie de la brute', 'Voie du pagne', 'Voie du pourfendeur', 'Voie du primitif', 'Voie de la rage']
+    },
+    barde: {
+        nom: 'Barde',
+        voies: ['Voie de l’escrime', 'Voie du musicien', 'Voie du saltimbanque', 'Voie de la séduction', 'Voie du vagabond']
+    },
+    chevalier: {
+        nom: 'Chevalier',
+        voies: ['Voie du cavalier', 'Voie de la guerre', 'Voie du héros', 'Voie du meneur d’homme', 'Voie de la noblesse']
+    },
+    druide: {
+        nom: 'Druide',
+        voies: ['Voie des animaux', 'Voie du fauve', 'Voie de la nature', 'Voie du protecteur', 'Voie des végétaux']
+    },
+    ensorceleur: {
+        nom: 'Ensorceleur',
+        voies: ['Voie de l’air', 'Voie de la divination', 'Voie de l’envoûteur', 'Voie des illusions', 'Voie de l’invocation']
+    },
+    forgesort: {
+        nom: 'Forgesort',
+        voies: ['Voie des artefacts', 'Voie des élixirs', 'Voie du métal', 'Voie du golem', 'Voie des runes']
+    },
+    guerrier: {
+        nom: 'Guerrier',
+        voies: ['Voie du bouclier', 'Voie du combat', 'Voie du maître d’armes', 'Voie de la résistance', 'Voie du soldat']
+    },
+    magicien: {
+        nom: 'Magicien',
+        voies: [
+            'Voie de la magie des arcanes',
+            'Voie de la magie destructrice',
+            'Voie de la magie élémentaire',
+            'Voie de la magie protectrice',
+            'Voie de la magie universelle'
+        ]
+    },
+    moine: {
+        nom: 'Moine',
+        voies: ['Voie de l’énergie vitale', 'Voie de la maîtrise', 'Voie de la méditation', 'Voie du poing', 'Voie du vent']
+    },
+    necromancien: {
+        nom: 'Nécromancien',
+        voies: ['Voie du démon', 'Voie de la mort', 'Voie de l’outre-tombe', 'Voie du sang', 'Voie de la sombre magie']
+    },
+    pretre: {
+        nom: 'Prêtre',
+        voies: ['Voie de la foi', 'Voie de la guerre sainte', 'Voie de la prière', 'Voie des soins', 'Voie de la spiritualité']
+    },
+    rodeur: {
+        nom: 'Rôdeur',
+        voies: [
+            'Voie de l’archer',
+            'Voie du compagnon animal',
+            'Voie de l’escarmouche',
+            'Voie de la survie',
+            'Voie du traqueur',
+            'Voie du montreur d’ours'
+        ]
+    },
+    voleur: {
+        nom: 'Voleur',
+        voies: ['Voie de l’assassin', 'Voie de l’aventurier', 'Voie du déplacement', 'Voie du roublard', 'Voie du spadassin']
+    }
+};
+
+const voies = {};
+
+module.exports = co;
